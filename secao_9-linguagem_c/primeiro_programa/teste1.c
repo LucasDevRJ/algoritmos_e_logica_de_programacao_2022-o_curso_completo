80f80dc8e3f12ab13efbcaa603d8e28691410ee7
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-14 21:09:12 -0300

    Criação de variável e atribuição de valor.

--- secao_9-linguagem_c/primeiro_programa/teste1.c
+++ secao_9-linguagem_c/primeiro_programa/teste1.c
@@ -1,7 +1,9 @@
+//Importação de biblioteca com funções de entrada/saída de dados
 #include <stdio.h>
 
 int main() {
 
+    //Exibe uma mensagem no console
     printf("Ola Mundo!");
 
     return 0;
---
+++ secao_9-linguagem_c/tipos_de_dados_e_variaveis/programa.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    //Criação de variável com seu tipo de dado
+    int x;
+
+    //Atribuição de valor a variável
+    //Não há atribuição de valor inicial
+    x = 10;
+
+    //Formatação (tipo inteiro) do valor da variável
+    printf("%d", x);
+
+    return 0;
+}
