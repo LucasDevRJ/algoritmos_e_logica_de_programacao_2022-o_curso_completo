a4ba83f7637bb3ca2b70e8b6d795fa04d0afc56a
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-16 17:48:37 -0300

    Exemplo de entrada de dados com C.

---
+++ secao_9-linguagem_c/entrada_de_dados/programa.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+
+    int idade;
+    double salario, altura;
+    char genero;
+    char nome[50];
+
+    printf("Digite o nome da pessoa: ");
+    scanf("%s", nome);
+
+    printf("Digite o valor da idade: ");
+    scanf("%d", &idade);
+
+    printf("Digite o valor do salario: ");
+    scanf("%lf", &salario);
+
+    printf("Digite o valor da altura: ");
+    scanf("%lf", &altura);
+
+    printf("NOME = %s\n", nome);
+    printf("IDADE = %d\n", idade);
+    printf("SALARIO = %.2lf\n", salario);
+    printf("ALTURA = %.2lf\n", altura);
+
+    return 0;
+}
