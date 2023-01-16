18c4dcbe99ddfb7ec15bdbab7a17ef26fdbee3d5
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-16 13:03:19 -0300

    Exemplo de saída de dados com formatação númerica.

---
+++ secao_9-linguagem_c/saida_de_dados/programa3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    int x, y;
+
+    x = 10;
+    y = 20;
+
+    //formatação numerica do tipo inteiro
+    printf("%d\n", x);
+    printf("%d\n", y);
+
+    return 0;
+}
