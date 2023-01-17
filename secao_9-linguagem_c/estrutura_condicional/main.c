baafa74fab74d13f8c17f6a8da436fb67724c8e1
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-17 15:18:39 -0300

    Exemplo de estrutura condicional usando C.

---
+++ secao_9-linguagem_c/estrutura_condicional/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int hora;
+
+    printf("Digite uma hora do dia: ");
+    scanf("%d", &hora);
+
+    if (hora < 12) {
+        printf("Bom dia!\n");
+    } else {
+        printf("Boa tarde!\n");
+    }
+
+    return 0;
+}
