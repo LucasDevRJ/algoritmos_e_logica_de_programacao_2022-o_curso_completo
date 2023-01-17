3aee2101a017e63b9bd8b7250be496b00973807c
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-17 15:27:58 -0300

    Exemplo de estrutura condicional while em C.

---
+++ secao_9-linguagem_c/estrutura_enquanto/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int x, soma;
+
+    soma = 0;
+
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &x);
+
+    while (x != 0) {
+        soma = soma + x;
+        printf("Digite outro numero: ");
+        scanf("%d", &x);
+    }
+
+    printf("SOMA = %d\n", soma);
+
+    return 0;
+}
