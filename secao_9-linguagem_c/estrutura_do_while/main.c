47060a6edd7991b730c79ab624e20f86805b25f5
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-19 19:18:53 -0300

    Exemplo da estrutura do while em C.

---
+++ secao_9-linguagem_c/estrutura_do_while/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void limpar_entrada() {
+    char c;
+    while ((c = getchar()) != '\n' && c != EOF) {}
+}
+
+int main()
+{
+
+    double C, F;
+    char resp;
+
+    do {
+
+        printf("Digite a temperatura em Celsius: ");
+        scanf("%lf", &C);
+
+        F = 9.0 * C / 5.0 + 32.0;
+
+        printf("Equivalente em Fahrenheit: %.1lf\n", F);
+
+        printf("Deseja repetir? (s/n)");
+        limpar_entrada();
+        scanf("%c", &resp);
+
+    } while (resp == 's');
+
+    return 0;
+}
