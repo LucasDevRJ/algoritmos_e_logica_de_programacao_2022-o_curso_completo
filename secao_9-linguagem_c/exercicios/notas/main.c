04d71993f13c535622c8d02bd63534ebedb03ad3
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-20 20:28:29 -0300

    Exercício que calcula a média de duas notas.

---
+++ secao_9-linguagem_c/exercicios/notas/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double nota1, nota2, notaFinal;
+
+    printf("Digite a primeira nota: ");
+    scanf("%lf", &nota1);
+
+    printf("Digite a segunda nota: ");
+    scanf("%lf", &nota2);
+
+    notaFinal = nota1 + nota2;
+
+    printf("\nNOTA FINAL = %.1lf", notaFinal);
+
+    if (notaFinal < 60.0) {
+        printf("\nREPROVADO");
+    }
+
+    return 0;
+}
