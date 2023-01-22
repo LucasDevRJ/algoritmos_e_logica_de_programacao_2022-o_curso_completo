6de4d3de0563855096637738b128c12973b22edd
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-22 18:14:07 -0300

    Exercício que recebe valores inteiros positivos, os soma e tira a média.

---
+++ secao_9-linguagem_c/exercicios/media_idades/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int valor, soma, contador;
+    double media;
+
+    printf("Digite as idades: \n");
+    scanf("%d", &valor);
+
+    soma = 0;
+    contador = 0;
+    if (valor < 0) {
+        printf("IMPOSSIVEL CALCULAR");
+    } else {
+        while (valor >= 0) {
+            soma = soma + valor;
+            contador++;
+            scanf("%d", &valor);
+        }
+    }
+
+    media = (double) soma / contador;
+    printf("MEDIA = %.2lf", media);
+
+    return 0;
+}
